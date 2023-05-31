fee28e09f0a80bc8631e1d50bc18263417e3096c
Atsuki02 <canvanvam@gmail.com>
2023-05-31 13:08:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+class Beberage {
+  constructor(name, price, imgUrl) {
+    this.name = name;
+    this.price = price;
+    this.imgUrl = imgUrl;
+  }
+}
+
+let Beberage = [
+  new Beberage("aquarius", 100, "img\2-top-aquarius.png"),
+  new Beberage("ayataka", 110, "img\2-top-ayataka.png"),
+  new Beberage("canada-dry", 120, "img\2-top-canada-dry.png"),
+  new Beberage("cocacola", 130, "img\2-top-cocacola.png"),
+  new Beberage("costa-v2", 140, "img\2-top-costa-v2.png"),
+  new Beberage("drpepper", 110, "img\2-top-drpepper.png"),
+  new Beberage("fanta", 120, "img\2-top-fanta.png"),
+  new Beberage("georgia", 130, "img\2-top-georgia.png"),
+  new Beberage("hajime", 140, "img\2-top-hajime.png"),
+  new Beberage("huang", 140, "img\2-top-huang.png"),
+  new Beberage("i-lohas", 170, "img\2-top-i-lohas.png"),
+  new Beberage("jackdanielcocacola", 140, "img\2-top-jackdanielcocacola.png"),
+  new Beberage("karadasukoyakacha", 150, "img\2-top-karadasukoyakacha.png"),
+  new Beberage("kochakaden", 160, "img\2-top-kochakaden.png"),
+  new Beberage("lemondo", 180, "img\2-top-lemondo.png"),
+  new Beberage("nomels", 160, "img\2-top-nomels.png"),
+  new Beberage("qoo", 180, "img\2-top-qoo.png"),
+  new Beberage("realgold", 170, "img\2-top-realgold.png"),
+  new Beberage("saryusaisai", 150, "img\2-top-saryusaisai.png"),
+  new Beberage("sokenbicha", 120, "img\2-top-sokenbicha.png"),
+  new Beberage("sprite", 100, "img\2-top-sprite.png"),
+  new Beberage("yakan-v2", 170, "img\2-top-yakan-v2.png"),
+  new Beberage("yogurstand", 130, "img\2-top-yogurstand.png"),
+  new Beberage("yowanai_lemondo", 160, "img\2-top-yowanai_lemondo.png"),
+];
